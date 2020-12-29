9ed3cf3503549f955ce00c210db905e68074bff1
Aaron Stroud <astroud@gmail.com>
2020-12-28 18:06:08 -0800

    Refactored Content component to render three Part components which handle rendering the name and number of exercises

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,8 +5,17 @@ const Header = (props) => {
   return(<h1>{props.course}</h1>)
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return(<p>{props.part} {props.exercises}</p>)
+}
+
+const Content = (props) => {
+  return(
+  <div>
+    <Part part={props.parts[0]} exercises={props.exercises[0]} />
+    <Part part={props.parts[1]} exercises={props.exercises[1]} />
+    <Part part={props.parts[2]} exercises={props.exercises[2]} />
+  </div>)
 }
 
 const Total = (props) => {
@@ -27,9 +36,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total  exercises1={exercises1}
               exercises2={exercises2}
               exercises3={exercises3} />
