45562ef0a6bc9aca3cf20da1cf9003b3757519fe
Aaron Stroud <astroud@gmail.com>
2020-12-28 17:43:29 -0800

    Refactored into three new components: Header, Content, and Total

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return(<h1>{props.course}</h1>)
+}
+
+const Content = (props) => {
+  return(<p>{props.part} {props.exercises}</p>)
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,17 +26,13 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total  exercises1={exercises1}
+              exercises2={exercises2}
+              exercises3={exercises3} />
     </div>
   )
 }
