2a03bb511875b50cf7f800251f5a3e848c49291e
Aaron Stroud <astroud@gmail.com>
2021-01-06 12:33:50 -0800

    feat: 2.6: The Phonebook Step1 - Add names to phone book

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,47 @@
 import React, { useState } from 'react'
 
+const Directory = ({ people }) => {
+  return(
+    <ul>
+      {people.map(person =>
+        <li key={person.name}>{person.name}</li>
+      )}
+    </ul>
+  )
+}
+
 const App = () => {
-  const [ persons, setPersons ] = useState([
+  const [ people, setPeople ] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('setPeople should save:', newName)
+    setPeople(people.concat({name: newName}))
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input
+                  value={newName}
+                  onChange={handlePersonChange}
+                />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Directory people={people} />
     </div>
   )
 }
