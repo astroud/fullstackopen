38258c97894f1184432c627571cc21aa3fdb9387
Aaron Stroud <astroud@gmail.com>
2021-01-05 21:30:53 -0800

    feat: 2.4: Course information step9, 2.5: separate module - handle arbitrary number of courses

--- part2/courseinfo/src/Components/Course.js
+++ part2/courseinfo/src/Components/Course.js
@@ -3,13 +3,17 @@ import Header from './Header';
 import Content from './Content';
 import Total from './Total';
 
-const Course = ({course}) => {
+const Course = ({courses}) => {
   return(
-    <>
-      <Header title={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
+    courses.map( course => {
+      return(
+        <div key={course.id}>
+          <Header title={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      )
+    }) 
   )
 }
 
