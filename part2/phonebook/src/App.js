3bf9fee8407d03330764d2e8b02679ada2ede738
Aaron Stroud <astroud@gmail.com>
2021-01-06 13:35:37 -0800

    fix: duplicateName correctly matches duplicates

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
 
   const duplicateName = (newName, savedNames) => {
     let filteredNames = savedNames.filter(
-          savedName => savedName.name.includes(newName))
+          savedName => savedName.name === newName)
     return filteredNames.length === 0 ? false : true 
   }
 
