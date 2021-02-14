6df7315acd1e5e9f2cdd6cf82ddf78c38fb09758
Aaron Stroud <astroud@gmail.com>
2021-02-13 16:01:42 -0800

    chore: partial commit from 3b. SEE DETAILS
    Created new repos for lesson's front and back ends
    as well as for the assignments. New repos
    simplified deployments.

--- part3/lesson-backend/index.js
+++ part3/lesson-backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 
-
+app.use(cors())
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
@@ -98,7 +99,7 @@ let notes = [
   
   app.use(unknownEndpoint)
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
